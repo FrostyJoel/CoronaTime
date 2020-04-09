b8bed71db86b255a3b38d2e485dcde9b2c590337
LAPTOP-C8F0QODP\Joël <Jsmjohn@hotmail.com>
2020-04-09 16:16:09 +0200

    Photon Testing

---
+++ CoronaTime(TempName)/Assets/MenuLogic.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuLogic : MonoBehaviour
+{
+    public GameObject connectedMenu;
+
+    public void DisableMenuUI()
+    {
+        PhotonNetwork.LoadLevel("Scene_Joël");
+    }
+}
---
+++ CoronaTime(TempName)/Assets/PhotonButtons.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonButtons : MonoBehaviour
+{
+    public MenuLogic mLogic;
+    public InputField createRoomInput, joinRoomInput;
+
+    public void OnClickCreateRoom()
+    {
+        if(createRoomInput.text.Length >= 1)
+        {
+            PhotonNetwork.CreateRoom(createRoomInput.text, new RoomOptions() { MaxPlayers = 4 }, null);
+        }
+    }
+
+    public void OnClickJoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinRoomInput.text);
+    }
+
+    private void OnJoinedRoom()
+    {
+        mLogic.DisableMenuUI();
+        Debug.Log("We are connected to the room!");
+    }
+    
+}
