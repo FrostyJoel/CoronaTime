df8de1afbee1e2c3b040246deefcccbdda8a75ae
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-04-15 13:01:42 +0200

    bool

--- CoronaTime(TempName)/Assets/Code/MeshStuff/MeshCombiner.cs
+++ CoronaTime(TempName)/Assets/Code/MeshStuff/MeshCombiner.cs
@@ -6,42 +6,86 @@ using System.Collections.Generic;
 // CombineInstance stores the list of meshes.  These are combined
 // and assigned to the attached Mesh.
 
+[ExecuteInEditMode]
 public class MeshCombiner : MonoBehaviour
 {
-    void Start()
-    {
-        ActualMeshCombining();
+
+    public bool execute;
+
+    public List<Meshes> meshes = new List<Meshes>();
+
+    void Update() {
+        if (execute) {
+            CombineMesh();
+            execute = false;
+        }
     }
 
-    public void ActualMeshCombining()
-    {
-        List<MeshFilter> meshFilters = new List<MeshFilter>();
-        MeshRenderer mR = gameObject.AddComponent<MeshRenderer>();
-        List<Material> matL = new List<Material>();
-        int i = 0;
-        for (int j = 0; j < transform.childCount; j++)
-        {
-            if (transform.GetChild(j).GetComponent<MeshFilter>())
-            {
-                Transform childJ = transform.GetChild(j);
-                meshFilters.Add(childJ.GetComponent<MeshFilter>());
-                matL.Add(childJ.GetComponent<MeshRenderer>().material);
+    void CombineMesh() {
+        CheckForMesh(transform);
+        for (int i = 0; i < meshes.Count; i++) {
+            InstanceFilterInstance(meshes[i]);
+        }
+    }
+
+    void InstanceFilterInstance(Meshes ms) {
+        CombineInstance[] combine = new CombineInstance[ms.meshList.Count];
+
+        Mesh combinedMesh = new Mesh();
+        combinedMesh.name = "Material:" + ms.meshList[0].name;
+        combinedMesh.CombineMeshes(combine, true, true, false);
+
+        GameObject g = new GameObject(ms.meshList[0].name);
+
+        MeshFilter filter = g.AddComponent<MeshFilter>();
+        filter.mesh = combinedMesh;
+
+        MeshRenderer render = g.AddComponent<MeshRenderer>();
+        render.material = ms.material;
+    }
+
+    void CheckForMesh(Transform t) {
+        if(t.GetComponent<MeshFilter>() && t.GetComponent<MeshRenderer>()) {
+            MeshFilter filter = t.GetComponent<MeshFilter>();
+            int index = MeshWhereInMeshes(filter.mesh);
+
+            if (index >= 0) {
+                meshes[index].meshList.Add(filter.mesh);
+            } else {
+                Meshes mesh = new Meshes();
+                mesh.meshList.Add(filter.mesh);
+                mesh.material = t.GetComponent<MeshRenderer>().material;
+                meshes.Add(mesh);
             }
         }
-        CombineInstance[] combine = new CombineInstance[meshFilters.Count];
-        mR.materials = matL.ToArray();
-        while (i < meshFilters.Count)
-        {
-            combine[i].mesh = meshFilters[i].sharedMesh;
-            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
-            DestroyImmediate(meshFilters[i]);
-            i++;
+
+        if (t.childCount > 0) {
+            for (int i = 0; i < t.childCount; i++) {
+                CheckForMesh(t.GetChild(i));
+            }
         }
-        Mesh mesh = new Mesh();
-        mesh.CombineMeshes(combine,false,true,false);
-        MeshFilter mF = gameObject.AddComponent<MeshFilter>();
-        mF.mesh = mesh;
-        transform.gameObject.SetActive(true);
-        print(gameObject.name + " Vertices: " + mF.mesh.vertices.Length);
     }
+    
+    int MeshWhereInMeshes(Mesh mesh) {
+        int i = -1;
+        if (meshes.Count > 0) {
+            for (int iB = 0; iB < meshes.Count; iB++) {
+                if (meshes[iB].meshList.Count > 0) {
+                    for (int iC = 0; iC < meshes[iB].meshList.Count; iC++) {
+                        if (mesh.name == meshes[iB].meshList[iC].name) {
+                            i = iB;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return i;
+    }
+}
+
+[System.Serializable]
+public class Meshes {
+    public List<Mesh> meshList = new List<Mesh>();
+    public Material material;
 }
