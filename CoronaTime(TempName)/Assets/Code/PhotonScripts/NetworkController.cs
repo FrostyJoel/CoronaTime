ae2bea081135bba849bb5494be63c3c03efe86d7
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-04-17 16:22:02 +0200

    quick photon save

---
+++ CoronaTime(TempName)/Assets/Code/PhotonScripts/NetworkController.cs
@@ -0,0 +1,14 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkController : MonoBehaviourPunCallbacks {
+    private void Start() {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster() {
+        print("Connected to " + PhotonNetwork.CloudRegion + " region");
+    }
+}
