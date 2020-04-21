22a204d81a2146e4a056eac938879a7c2219202d
Johannes <johanspeek@gmail.com>
2020-04-21 19:36:03 +0200

    layouts

---
+++ CoronaTime(TempName)/Assets/Code/UX UI/Options.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    public Toggle fullscreenToggle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fullscreenToggle.isOn = Screen.fullScreen;
+    }
+
+    public void SetFullscreen(bool fullscreenState)
+    {
+        Screen.fullScreen = fullscreenState; 
+    } 
+}
