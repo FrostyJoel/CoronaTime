e34fd97f2b5715cb8973a6037c9dc106cb988d23
LAPTOP-C8F0QODP\Joël <Jsmjohn@hotmail.com>
2020-04-25 11:59:26 +0200

    Outline Fix

--- CoronaTime(TempName)/Assets/Code/Outline/OutlineCheck.cs
+++ CoronaTime(TempName)/Assets/Code/Outline/OutlineCheck.cs
@@ -4,64 +4,32 @@ using UnityEngine;
 
 public class OutlineCheck : MonoBehaviour
 {
-    public float playerCheckRad = 5f;
-    public List<Outline> oLines = new List<Outline>();
-    public void Update()
+    public RaycastHit hit;
+    public Outline lastOutline;
+    private Controller controller;
+    private CartStorage storage;
+    private void Start()
     {
-        Collider[] objCol = Physics.OverlapSphere(transform.position, playerCheckRad);
-        for (int i = 0; i < objCol.Length; i++)
-        {
-            if (objCol[i].tag == "Interact" && objCol[i].GetComponent<Outline>())
-            {
-                Renderer render = objCol[i].GetComponent<Renderer>();
-                Outline oLine = objCol[i].GetComponent<Outline>();
-                InteractableProduct iProduct = objCol[i].GetComponent<InteractableProduct>();
-
-                if (iProduct)
-                {
-                    if(iProduct.currentPlace != InteractableProduct.Place.InShelve)
-                    {
-                        break;
-                    }
-                }
-                if (!oLines.Contains(oLine))
-                {
-                    oLines.Add(oLine);
-                }
-                if (render.isVisible)
-                {
-                    oLine.enabled = true;
-                }
-                else
-                {
-                    if (oLine.enabled)
-                    {
-                        oLine.enabled = false;
-                    }
-                }
-            }
-        }
-        if(oLines.Count > 0)
-        {
-            for (int i = 0; i < oLines.Count; i++)
-            {
-                if(oLines[i] == null)
-                {
-                    oLines.Remove(oLines[i]);
-                    return;
-                }
-                float dis = Vector3.Distance(transform.position, oLines[i].transform.position);
-                if (dis > playerCheckRad && oLines[i].enabled)
-                {
-                    oLines[i].enabled = false;
-                    oLines.Remove(oLines[i]);
-                }
-            }
-        }
+        controller = GetComponent<Controller>();
+        storage = GetComponent<CartStorage>();
     }
-    private void OnDrawGizmos()
+    private void Update()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, playerCheckRad);
+        if (Physics.Raycast(controller.pov.position, controller.pov.forward, out hit, storage.interactRange))
+        {
+            Outline outlineObj = hit.transform.gameObject.GetComponent<Outline>();
+            if (lastOutline != outlineObj && lastOutline != null)
+            {
+                lastOutline.enabled = false;
+            }
+            if (outlineObj)
+            {
+                if (!outlineObj.enabled)
+                {
+                    lastOutline = outlineObj;
+                    outlineObj.enabled = true;
+                }
+            }
+        }
     }
 }
