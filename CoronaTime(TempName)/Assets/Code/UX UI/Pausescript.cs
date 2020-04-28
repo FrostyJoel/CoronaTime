e374993d7877fcbb45e97ed80e9bfe5a9f4629c0
Johannes <johanspeek@gmail.com>
2020-04-28 21:24:06 +0200

    menu functionality + hierarchy cleanup

---
+++ CoronaTime(TempName)/Assets/Code/UX UI/Pausescript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pausescript : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Cancel"))
+        {
+            pauseMenu.SetActive(true);
+        }
+    }
+}
