f1333ce1c0c081da8fb777fb9d04d6f8b809d71d
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-04-29 09:31:23 +0200

    ui sound example

---
+++ CoronaTime(TempName)/Assets/Code/UX UI/SliderEvent.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class SliderEvent : MonoBehaviour, IPointerUpHandler {
+    [HideInInspector] public UiElement uiElement;
+    public void OnPointerUp(PointerEventData eventData) {
+        AudioManager.PlaySound(uiElement.audioClip, uiElement.audioGroup);
+    }
+}
---
+++ CoronaTime(TempName)/Assets/Code/UX UI/UiElementSounds.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UiElementSounds : MonoBehaviour {
+    public List<UiElement> element = new List<UiElement>();
+    private void Start() {
+        if (element.Count > 0) {
+            for (int i = 0; i < element.Count; i++) {
+                element[i].AddSoundListener(this.gameObject);
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class UiElement : IPointerUpHandler {
+    public enum type {
+        button,
+        toggle,
+        slider,
+        dropdown
+    }
+    public type ThisType;
+    public AudioClip audioClip;
+
+    public AudioManager.AudioGroups audioGroup;
+
+    [Header("Ui Elements")]
+    public Button button;
+    public Toggle toggle;
+    public Slider slider;
+    public Dropdown dropdown;
+
+    public void AddSoundListener(GameObject elementObject) {
+        switch (ThisType) {
+            case type.button:
+            if (!button && elementObject.GetComponent<Button>()) {
+                button = elementObject.GetComponent<Button>();
+            }
+            if (button) {
+                button.onClick.AddListener(() => AudioManager.PlaySound(audioClip, audioGroup));
+            } 
+            break;
+            case type.toggle:
+            if (!toggle && elementObject.GetComponent<Toggle>()) {
+                toggle = elementObject.GetComponent<Toggle>();
+            }
+            if (toggle) {
+                toggle.onValueChanged.AddListener(OnToggleChange);
+            }
+            break;
+            case type.slider:
+            if(!slider && elementObject.GetComponent<Slider>()) {
+                slider = elementObject.GetComponent<Slider>();
+            }
+            if (slider) {
+                SliderEvent se = slider.gameObject.AddComponent<SliderEvent>();
+                se.uiElement = this;
+            }
+            break;
+            case type.dropdown:
+            if(!dropdown && elementObject.GetComponent<Dropdown>()) {
+                dropdown = elementObject.GetComponent<Dropdown>();
+            }
+            if (dropdown) {
+                dropdown.onValueChanged.AddListener(OnDropDownChanged);
+            }
+            break;
+        }
+    }
+
+    void OnToggleChange(bool b) {
+        AudioManager.PlaySound(audioClip, audioGroup);
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        Debug.Log("Sliding finished");
+    }
+
+    public void OnDropDownChanged(int i) {
+        AudioManager.PlaySound(audioClip, audioGroup);
+    }
+}
