126311de8a4f411e3ea151ca1f44e40f504c093f
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-05-09 09:00:58 +0200

    push

--- CoronaTime(TempName)/Assets/Code/PhotonPunRpcCallsExample.cs
+++ CoronaTime(TempName)/Assets/Code/PhotonPunRpcCallsExample.cs
@@ -1,14 +1,30 @@
 ﻿using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PhotonPunRpcCallsExample : MonoBehaviour {
     public static PhotonPunRpcCallsExample testSingle;
     public bool bull;
-
+    public Text text;
+    public bool lastBull = true;
     private void Awake() {
-        testSingle = this;
+        //testSingle = this;
+        if(PhotonPunRpcCallsExample.testSingle == null) {
+            testSingle = this;
+        } else if (PhotonPunRpcCallsExample.testSingle != this) {
+            Destroy(PhotonPunRpcCallsExample.testSingle.gameObject);
+            PhotonPunRpcCallsExample.testSingle = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Update() {
+        if (lastBull != PhotonPunRpcCallsExample.testSingle.bull) {
+            text.text = PhotonPunRpcCallsExample.testSingle.bull.ToString();
+            lastBull = PhotonPunRpcCallsExample.testSingle.bull;
+        }
     }
 
     [PunRPC]
@@ -18,6 +34,6 @@ public class PhotonPunRpcCallsExample : MonoBehaviour {
 
     [PunRPC]
     void Testt() {
-        bull = !bull;
+        PhotonPunRpcCallsExample.testSingle.bull = !PhotonPunRpcCallsExample.testSingle.bull;
     }
 }
