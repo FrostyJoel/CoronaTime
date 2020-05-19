b8b15aef9c34cc41028d78ecb191275bd5eb83ab
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-05-19 14:07:31 +0200

    rip

--- CoronaTime(TempName)/Assets/Code/ZoneControl.cs
+++ CoronaTime(TempName)/Assets/Code/ZoneControl.cs
@@ -1,10 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
 
 public class ZoneControl : MonoBehaviour {
-    public ZoneType[] zoneTypes = new ZoneType[4];
+    public static ZoneControl zcSingle;
+    public List<ZoneType> zoneTypes = new List<ZoneType>();
+    [HideInInspector] public List<int> order = new List<int>();
+
+    private void Awake() {
+        zcSingle = this;
+    }
+
+    public void RandomizeOrder() {
+        order.Clear();
+        for (int i = 0; i < zoneTypes.Count; i++) {
+            order.Add(i);
+        }
+        for (int i = 0; i < order.Count; i++) {
+            int temp = order[i];
+            int randomIndex = Random.Range(i, order.Count);
+            order[i] = order[randomIndex];
+            order[randomIndex] = temp;
+        }
+    }
 }
 
 [System.Serializable]
