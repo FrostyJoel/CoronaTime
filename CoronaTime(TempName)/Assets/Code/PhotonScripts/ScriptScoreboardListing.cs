2447b172d7a6f19f074660acbc971f4a86978e10
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-05-20 13:46:12 +0200

    ui

--- CoronaTime(TempName)/Assets/Code/PhotonScripts/ScriptScoreboardListing.cs
+++ CoronaTime(TempName)/Assets/Code/PhotonScripts/ScriptScoreboardListing.cs
@@ -6,5 +6,5 @@ using UnityEngine;
 [System.Serializable]
 public class ScriptScoreboardListing : MonoBehaviour {
     public int id;
-    public Text text_Username, text_Score;
+    public Text text_Username, text_ItemsInCart, text_Score;
 }
