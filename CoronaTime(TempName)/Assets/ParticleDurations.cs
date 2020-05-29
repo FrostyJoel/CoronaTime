efc42e6d5265f34fd3c864003c29c7eb5bf23eb7
J John <Jsmjohn@hotmail.com>
2020-05-29 14:58:22 +0200

    Particles

---
+++ CoronaTime(TempName)/Assets/ParticleDurations.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDurations : MonoBehaviour
+{
+    public float dur = 3f;
+    public bool play = false;
+    private ParticleSystem[] ps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponentsInChildren<ParticleSystem>();
+        foreach (ParticleSystem party in ps)
+        {
+            ParticleSystem.MainModule nPS = party.main;
+            nPS.duration = dur;   
+        }
+    }
+    private void Update()
+    {
+        if (play)
+        {
+            foreach (ParticleSystem party in ps)
+            {
+                party.Stop();
+                party.Play();
+            }
+            play = false;
+        }
+    }
+}
