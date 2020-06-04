db030c779f9ef98f6fe406dca210f714d3cee55b
Johannes <johanspeek@gmail.com>
2020-06-04 13:00:19 +0200

    push + menu updates
    max, u right

---
+++ CoronaTime(TempName)/Assets/Code/UX UI/PlayAnimOnEnable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimOnEnable : MonoBehaviour
+{
+    public Animator anim;
+
+    public void Awake()
+    {
+        anim.SetTrigger("Start");
+    }
+
+    private void OnEnable()
+    {
+        anim.SetTrigger("Start");
+    }
+
+    private void OnDisable()
+    {
+        anim.SetTrigger("End");
+    }
+}
