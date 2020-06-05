a893ee79233570cec0c07e8607123ee9d88754fb
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-06-05 16:35:40 +0200

    requiring grocerylist

--- CoronaTime(TempName)/Assets/Code/ZoneControl/ZoneControl.cs
+++ CoronaTime(TempName)/Assets/Code/ZoneControl/ZoneControl.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(GroceryList))]
 public class ZoneControl : MonoBehaviour {
     public static ZoneControl zc_Single;
     public Zone[] zones;
