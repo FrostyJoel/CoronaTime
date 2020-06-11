5d2d68924deed11b72cbbfe5b7a65ecc3c9372ad
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-06-11 12:01:16 +0200

    screen shot

---
+++ CoronaTime(TempName)/Assets/Code/ScreenCapTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenCapTest : MonoBehaviour {
+    int i;
+    public bool ss;
+    private void Start() {
+        Debug.LogWarning(Application.dataPath);
+    }
+
+    private void Update() {
+        if (ss) {
+            ScreenCapture.CaptureScreenshot("Assets/ScreenShotTest"+i+".png");
+            ss = false;
+            i++;
+        }
+    }
+
+}
