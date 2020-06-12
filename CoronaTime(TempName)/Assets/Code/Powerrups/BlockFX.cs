5b16b720383e1d77e9c8737eb3f6f3f93a02a9cb
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-06-12 16:33:31 +0200

    stoopid me

--- CoronaTime(TempName)/Assets/Code/Powerrups/BlockFX.cs
+++ CoronaTime(TempName)/Assets/Code/Powerrups/BlockFX.cs
@@ -4,19 +4,18 @@ using Photon.Pun;
 public class BlockFX : PowerUp {
     public override void Effect() {
         if (!inUse) {
+            ShowFX();
             List<PowerUp> pus = new List<PowerUp>();
             for (int i = 0; i < affectedController.powerups_AffectingMe.Count; i++) {
                 pus.Add(affectedController.powerups_AffectingMe[i]);
             }
             pus.Remove(this);
             if (pus.Count > 0) {
-                HideFX();
                 pus[0].StopUsing();
                 StopUsing();
             } else {
                 StartStopParticle(true);
             }
-            ShowFX();
             ProductInteractions.pi_Single.DisableVisibility(index, affectedController.photonView.ViewID, false, RpcTarget.All);
             inUse = true;
         }
@@ -24,6 +23,7 @@ public class BlockFX : PowerUp {
 
     public override void StopUsing() {
         PlayFXSound();
+        HideFX();
         base.StopUsing();
     }
 }
