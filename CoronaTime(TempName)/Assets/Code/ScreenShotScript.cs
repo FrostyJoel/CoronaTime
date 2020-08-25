b72f184a9a3aba58cbd7e4dab122722aa1a4dfa7
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-08-25 11:06:24 +0200

    screen shot
    - Edit ScreenCapCapt > ScreenShotScript (script)
    ^ it's now an editor tool :confetti_ball:

---
+++ CoronaTime(TempName)/Assets/Code/ScreenShotScript.cs
@@ -0,0 +1,32 @@
+﻿// Made by Max Broijl
+// Editor tool to make screenshots in editor
+// Resolution comes from editor resolution
+
+using UnityEngine;
+using UnityEditor;
+
+public class ScreenShotScript : MonoBehaviour {
+
+    public void MakeScreenShot() {
+        print(Application.dataPath);
+        ScreenCapture.CaptureScreenshot("Assets/ScreenShotTest" + Random.Range(100000000, 999999999) + ".png");
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(ScreenShotScript))]
+public class ScreenShotButton : Editor {
+    ScreenShotScript target_ScreenShot;
+    public void OnEnable() {
+        target_ScreenShot = (ScreenShotScript)target;
+    }
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+        if (GUILayout.Button("Make screenshot")) {
+            target_ScreenShot.MakeScreenShot();
+            Debug.LogWarning("Successfully made a screenshot!");
+        }
+    }
+
+}
+#endif
