c4058343833d8da1726015cfe5976d4ef9c84e80
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-08-26 11:19:11 +0200

    Steam
    - Add Steam (plugin)
    - Add SteamTest (script)
    ^ to test if connected to Steam (steam has to be running in background)
    by showing friends list

---
+++ CoronaTime(TempName)/Assets/Code/SteamScripts/SteamTest.cs
@@ -0,0 +1,19 @@
+﻿////////////////////////////////////////////////////////
+//Steam has to be running in background for this to work
+////////////////////////////////////////////////////////
+using Steamworks;
+using UnityEngine;
+
+public class SteamTest : MonoBehaviour {
+
+    private void Start() {
+        if(!SteamManager.Initialized) { return; }
+        string myName = SteamFriends.GetPersonaName();
+        int friendCount = SteamFriends.GetFriendCount(EFriendFlags.k_EFriendFlagImmediate);
+        for (int i = 0; i < friendCount; i++) {
+            CSteamID friend = SteamFriends.GetFriendByIndex(i, EFriendFlags.k_EFriendFlagImmediate);
+            string friendName = SteamFriends.GetFriendPersonaName(friend);
+            print(myName + " is friends with " + friendName);
+        }
+    }
+}
